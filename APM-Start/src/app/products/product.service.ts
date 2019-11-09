bc5abcb1dc3a7911cf1f657e7ce7c5f4337302a0
Inventrohyder <haitham.hyder@gmail.com>
2019-11-10 03:41:15 +0900

    Add exception handling to the observable
    We still need to subscribe to see the data.
    However, error handling better prepares us for problems that may occur.

--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from "@angular/core";
 import { IProduct } from './product';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
@@ -14,7 +15,26 @@ export class ProductService{
     }
 
     getProducts(): Observable<IProduct[]> {
-        return this.http.get<IProduct[]>(this.productsUrl);
+        return this.http.get<IProduct[]>(this.productsUrl).pipe(
+            tap(data => console.log('All: ' + JSON.stringify(data))),
+            catchError(this.handleError)
+        );
+    }
+
+    private handleError(err: HttpErrorResponse){
+        // In a real world app, we may send the server to some remote logging infrastracture
+        // instead of just logging it to the console
+        let errorMessage = '';
+        if (err.error instanceof ErrorEvent){
+            // A client-side or network error occured. Handle it accordingly.
+            errorMessage = `An error occured: ${err.error.message}`
+        } else {
+            // The backend returned an unsuccessful response code.
+            // The response body may contain clues as to what went wrong.
+            errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+        }
+        console.error(errorMessage);
+        return throwError(errorMessage);
     }
 
 }
